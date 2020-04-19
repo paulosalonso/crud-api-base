b82002d8325528985d8b7d92ffb0b961addbe26e
Paulo Silas Alonso <paulo.silas@fluig.com>
2020-04-19 15:30:42 -0300

    Classe ExampleControllerCreateIT renomeadapara ExampleControllerIT, para centralizar todos os testes, uma vez que alguns testes cobrem mais de um método da API e se repetiriam usando classes separadas.

--- src/test/java/com/alon/spring/crud/api/controller/ExampleControllerIT.java
+++ src/test/java/com/alon/spring/crud/api/controller/ExampleControllerIT.java
@@ -1,12 +1,15 @@
 package com.alon.spring.crud.api.controller;
 
-import com.alon.spring.crud.cleaner.DatabaseCleaner;
-import com.alon.spring.crud.domain.model.Example;
-import com.alon.spring.crud.domain.service.CrudService;
-import com.alon.spring.crud.domain.service.ExampleService;
+import static com.alon.spring.crud.domain.service.CrudService.HookHelper.LifeCycleHook.AFTER_CREATE;
+import static com.alon.spring.crud.domain.service.CrudService.HookHelper.LifeCycleHook.BEFORE_CREATE;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,15 +20,14 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
-import static com.alon.spring.crud.domain.service.CrudService.HookHelper.LifeCycleHook.AFTER_CREATE;
-import static com.alon.spring.crud.domain.service.CrudService.HookHelper.LifeCycleHook.BEFORE_CREATE;
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
-import static org.hamcrest.Matchers.*;
+import com.alon.spring.crud.cleaner.DatabaseCleaner;
+import com.alon.spring.crud.domain.model.Example;
+import com.alon.spring.crud.domain.service.ExampleService;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -64,13 +66,13 @@ public class ExampleControllerCreateIT {
                 .post("/example")
         .then()
                 .statusCode(HttpStatus.CREATED.value())
-                .body("id", allOf(notNullValue(), equalTo(1)))
+                .body("id", notNullValue())
                 .body("stringProperty", equalTo(example.getStringProperty()));
     }
 
     @Test
     public void whenCreateWithBeforeCreateHookThenPersistWithHookUpdates() {
-        exampleService.addBeforeCreateHook(this::concatStringProperty);
+        exampleService.addBeforeCreateHook(this::concatStringPropertyCreate);
 
         Example example = Example.of()
                 .stringProperty("property")
@@ -98,27 +100,30 @@ public class ExampleControllerCreateIT {
 
     @Test
     public void whenCreateWithAfterCreateHookThenPersistOriginalAndReturnWithHookUpdates() {
-        exampleService.addAfterCreateHook(this::concatStringProperty);
+        exampleService.addAfterCreateHook(this::concatStringPropertyCreate);
 
         Example example = Example.of()
                 .stringProperty("property")
                 .build();
 
-        given()
+        Response response = given()
                 .contentType(ContentType.JSON)
                 .body(example)
-        .when()
-                .post("/example")
-        .then()
+                .when()
+                .post("/example");
+
+        Example responseExample = response.body().as(Example.class);
+
+        response.then()
                 .statusCode(HttpStatus.CREATED.value())
-                .body("id", equalTo(1))
+                .body("id", notNullValue())
                 .body("stringProperty", equalTo(example.getStringProperty().concat("-concatenated-by-hook")));
 
         when()
-                .get("/example/{id}", 1)
-        .then()
+                .get("/example/{id}", responseExample.getId())
+                .then()
                 .statusCode(HttpStatus.OK.value())
-                .body("id", equalTo(1))
+                .body(notNullValue())
                 .body("stringProperty", equalTo(example.getStringProperty()));
 
         exampleService.clearHooks(AFTER_CREATE);
@@ -131,7 +136,7 @@ public class ExampleControllerCreateIT {
                 .body("content", hasSize(0));
     }
 
-    private Example concatStringProperty(Example example) {
+    private Example concatStringPropertyCreate(Example example) {
         example.setStringProperty(example
                 .getStringProperty().concat("-concatenated-by-hook"));
 
