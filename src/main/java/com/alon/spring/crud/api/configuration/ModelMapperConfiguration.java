363438a15577475036cccba630e97d16d43d9706
Paulo Alonso <paulo_alonso_@hotmail.com>
2020-05-10 20:57:28 -0300

    Configuração do ModelMapper para ignorar relacionamentos lazy para List e Set.

--- src/main/java/com/alon/spring/crud/api/configuration/ModelMapperConfiguration.java
+++ src/main/java/com/alon/spring/crud/api/configuration/ModelMapperConfiguration.java
@@ -1,13 +1,18 @@
 package com.alon.spring.crud.api.configuration;
 
-import org.hibernate.collection.internal.AbstractPersistentCollection;
+import org.hibernate.collection.internal.PersistentList;
+import org.hibernate.collection.internal.PersistentSet;
 import org.modelmapper.Condition;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.Provider;
 import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class ModelMapperConfiguration {
@@ -17,9 +22,20 @@ public class ModelMapperConfiguration {
         ModelMapper modelMapper = new ModelMapper();
 
         modelMapper.getConfiguration()
-                .setFieldMatchingEnabled(true)
-                .setFieldAccessLevel(PRIVATE);
+                .setPropertyCondition(buildCondition());
 
         return modelMapper;
+    }
+
+    private Condition buildCondition() {
+        return context -> {
+            if (context.getSourceType().equals(PersistentSet.class))
+                return ((PersistentSet) context.getSource()).wasInitialized();
+
+            if (context.getSourceType().equals(PersistentList.class))
+                return ((PersistentList) context.getSource()).wasInitialized();
+
+            return true;
+        };
     }
 }
