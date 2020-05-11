4faae0f9c3c8a1b98112f9d67f7f56ea25386b0e
Paulo Alonso <paulo_alonso_@hotmail.com>
2020-05-11 14:52:21 -0300

    Melhoria na verificação de atributos lazy com ModelMapper.

--- src/main/java/com/alon/spring/crud/api/configuration/ModelMapperConfiguration.java
+++ src/main/java/com/alon/spring/crud/api/configuration/ModelMapperConfiguration.java
@@ -1,18 +1,10 @@
 package com.alon.spring.crud.api.configuration;
 
-import org.hibernate.collection.internal.PersistentList;
-import org.hibernate.collection.internal.PersistentSet;
-import org.modelmapper.Condition;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.Provider;
-import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import static org.hibernate.Hibernate.isInitialized;
 
 @Configuration
 public class ModelMapperConfiguration {
@@ -22,20 +14,8 @@ public class ModelMapperConfiguration {
         ModelMapper modelMapper = new ModelMapper();
 
         modelMapper.getConfiguration()
-                .setPropertyCondition(buildCondition());
+                .setPropertyCondition(context -> isInitialized(context.getSource()));
 
         return modelMapper;
-    }
-
-    private Condition buildCondition() {
-        return context -> {
-            if (context.getSourceType().equals(PersistentSet.class))
-                return ((PersistentSet) context.getSource()).wasInitialized();
-
-            if (context.getSourceType().equals(PersistentList.class))
-                return ((PersistentList) context.getSource()).wasInitialized();
-
-            return true;
-        };
     }
 }
